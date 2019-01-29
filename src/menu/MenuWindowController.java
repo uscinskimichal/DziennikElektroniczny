e042242ec5b84e2add017daf043c1aa5cc705aca
Michał <uscinskimichal@gmail.com>
2019-01-29 02:49:39 +0100

    added api class

--- src/menu/MenuWindowController.java
+++ src/menu/MenuWindowController.java
@@ -1,5 +1,6 @@
 package menu;
 
+import main.IpInformation;
 import navigator.Navigator;
 import userInformations.UserLoggedIn;
 import javafx.application.Platform;
@@ -23,40 +24,8 @@ public class MenuWindowController extends Navigator implements Initializable {
         userCountry.setText(UserLoggedIn.COUNTRY);
     }
 
-    private void threadsAPI() {
-        new Thread(() -> {
-            try {
-                UserLoggedIn.IP = getIp();
-
-                Semaphore semaphore = new Semaphore(-1);
-
-                new Thread(() -> {
-                    try {
-                        UserLoggedIn.CITY = getData(UserLoggedIn.IP, "city");
-                        semaphore.release();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }).start();
-                new Thread(() -> {
-                    try {
-                        UserLoggedIn.COUNTRY = getData(UserLoggedIn.IP, "country_name");
-                        semaphore.release();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }).start();
-                semaphore.acquire();
-
-                Platform.runLater(() -> {
-                    userIp.setText(UserLoggedIn.IP);
-                    userCity.setText(UserLoggedIn.CITY);
-                    userCountry.setText(UserLoggedIn.COUNTRY);
-                });
-            } catch (IOException | InterruptedException e) {
-                e.printStackTrace();
-            }
-        }).start();
+    private void shootAPI() {
+        new IpInformation(userIp,userCity,userCountry);
     }
 
     private void printUser() {
@@ -66,25 +35,6 @@ public class MenuWindowController extends Navigator implements Initializable {
             classLabel.setText(UserLoggedIn.CLASS);
         }
         userLabel.setText(UserLoggedIn.NAME + " " + UserLoggedIn.SURNAME);
-    }
-
-    private String getData(String myIp, String data) throws IOException {
-
-        String output;
-        URL city = new URL("https://ipapi.co/" + myIp + "/" + data + "/");
-        BufferedReader cityBuffer = new BufferedReader(new InputStreamReader(city.openStream()));
-        output = cityBuffer.readLine();
-        System.out.println(output);
-        return output;
-    }
-
-    private String getIp() throws IOException {
-        String myIp;
-        URL url = new URL("https://ipapi.co/ip");
-        BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
-        myIp = br.readLine();
-        System.out.println(myIp);
-        return myIp;
     }
 
     @FXML
@@ -109,7 +59,7 @@ public class MenuWindowController extends Navigator implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         printUser();
         if (UserLoggedIn.IP == null)
-            threadsAPI();
+            shootAPI();
         else
             printNetwork();
     }
